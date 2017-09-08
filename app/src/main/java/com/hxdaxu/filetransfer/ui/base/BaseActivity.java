8e207777b4dfe0189e1c0ca54235cdeb4d5e8d7d
huangqingchun <huangqingchun@gionee.com>
2017-09-08 20:36:47 +0800

    init commit

---
+++ app/src/main/java/com/hxdaxu/filetransfer/ui/base/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.hxdaxu.filetransfer.ui.base;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public abstract class BaseActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
