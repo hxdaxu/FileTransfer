875b9599af5e71a7d229e0ccdaa194e1a3f8c8cf
huangqingchun <huangqingchun@gionee.com>
2017-09-17 11:01:02 +0800

    EventManager

--- app/src/main/java/com/hxdaxu/filetransfer/event/EventManager.java
+++ app/src/main/java/com/hxdaxu/filetransfer/event/EventManager.java
@@ -1,6 +1,13 @@
 package com.hxdaxu.filetransfer.event;
 
 
+import android.support.annotation.NonNull;
+
+import com.hxdaxu.filetransfer.ui.base.BaseActivity;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -8,7 +15,6 @@ import java.util.concurrent.ConcurrentHashMap;
 public class EventManager {
 
     private static EventManager eventManager;
-
 
     private EventManager(){
     }
@@ -28,10 +34,48 @@ public class EventManager {
     private Map<IEvent,List<EventListener>> listenerMap = new ConcurrentHashMap<IEvent,List<EventListener>>();
 
     // 注册方法
-    public void register(){
+    public void registerListener(List<IEvent> events,List<EventListener> listeners){
+        for (IEvent event : events){
+
+            if (null == listenerMap.get(event)){
+
+                //监听者列表使用有序列表，可以控制事件分发顺序
+                listenerMap.put(event,new ArrayList<EventListener>());
+            }
+
+            for (EventListener listener : listeners){
+
+                if(listenerMap.get(event).contains(listener)){
+                    // log.e  监听者已注册
+                } else {
+                    listenerMap.get(event).add(listener);
+                }
+
+            }
+
+            Collections.sort(listenerMap.get(event), new Comparator<EventListener>() {
+                @Override
+                public int compare(EventListener t0, EventListener t1) {
+                    return getPriority(t0).compareTo(getPriority(t1));
+                }
+            });
+
+
+
+
+        }
+
+
 
     }
 
-
+    private Integer getPriority(EventListener listener){
+        // 界面优先级高
+        if (listener instanceof BaseActivity){
+            return 1;
+        } else {
+            return 2;
+        }
+    }
 
 }
